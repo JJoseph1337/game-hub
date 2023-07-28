7fd16db61e4f0b146051858f4cb03df14509c8fd
JJoseph1337 <dassdass733@gmail.com>
2023-07-28 15:14:07 +0500

    Build a navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 import App from "./App";
 import "./index.css";
 
